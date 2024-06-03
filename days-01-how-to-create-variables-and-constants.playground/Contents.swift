37d3ea4384152dc6dc03ac1d7649efd208b0e837
Min Kaung Kyaw <minkgkyaw9899@gmail.com>
2024-06-03 18:19:36 +0630

    finished day1

---
+++ days-01-how-to-create-variables-and-constants.playground/Contents.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+//variable
+
+var name = "Mg Mg"
+
+name = "Aung Aung"
+
+var age = 24
+
+age = 20
+
+// let can't redeclear
+let dog = "Aung Net"
+
+print(dog)
+
+let movieName = """
+a day in
+tesing hello world
+testing
+"""
+
+print(movieName.count)
+
+let score = 20
+
+let bigScore = 200_000_000
+
+print(score - 10)
+
+let double = 20.20
+
+print (double)
+print(Double(double))
+
+let double2 = 3.0
+print(double2)
+print(Double(double2))
+
+var rating = 3
+
+rating += 3
+
