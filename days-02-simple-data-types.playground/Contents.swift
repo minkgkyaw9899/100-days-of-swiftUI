70731e042a1a56a3615eb324cf1819b1c5af602f
Min Kaung Kyaw <minkgkyaw9899@gmail.com>
2024-06-04 15:25:28 +0630

    finished day2

---
+++ days-02-simple-data-types.playground/Contents.swift
@@ -0,0 +1,31 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+//boolean data type
+
+var fileName = "Mkk.png"
+
+fileName.hasSuffix("png")
+
+var gameOver = false
+
+gameOver.toggle()
+
+print(gameOver)
+
+//join string together
+
+let firstName = "Min"
+let middlename = "Kaung"
+let lastName = "Kyaw"
+
+var name = firstName + middlename + lastName
+
+name = firstName + "Kaung" + lastName
+
+name = firstName + " " + middlename + " " + lastName
+
+let age = 24;
+
+print("My name is \(name) and age is \(age)")
